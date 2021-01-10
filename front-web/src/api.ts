3b46fd0ce65062b007beca1af6b3bcc1396780a2
Guilherme <guilherme-allves@hotmail.com>
2021-01-10 17:31:25 -0300

    front-web - Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const URL = 'https://gui-sds2.herokuapp.com';
+const URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
