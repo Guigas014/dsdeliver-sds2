b2caab9ce07df2bcc902f3ec16a6dd663ba75f64
Guilherme <guilherme-allves@hotmail.com>
2021-01-13 19:53:26 -0300

    Create front-web and page Home

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
+
+import Header from '../Header';
+
+
+export default function Orders() {
+  return (
+    <>
+      <Header />
+      <View>
+        <Text>Listagem de Pedidos</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  });
